c020f45784563fdf7bc191ada580cd1832e8a4c1
Nanda <58926775+gumelartresnadwinanda@users.noreply.github.com>
2025-04-11 19:03:50 +0700

    chore: add type field in tags table

---
+++ migrations/20250411120143_add_tags_type.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("tags", function (table) {
+    table.string("type");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("tags", function (table) {
+    table.dropColumn("type");
+  });
+};
